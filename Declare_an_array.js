94d57963594b15b21deac3eae098f13488350af5
Mosfaqur99 <you@example.com>
2025-02-10 21:27:01 +0600

    connected with my home laptop

--- Declare_an_array.js
+++ Declare_an_array.js
@@ -1,5 +1,6 @@
-const fruits = ["Lichi","Mango","Berry","Jack-Fruit","Banana"]
+const fruits = ["Lichi", "Mango", "Berry", "Jack-Fruit", "Banana"];
 
-fruits[2] = "jambura"
+fruits[2] = "jambura";
 console.log(fruits[2]);
 console.log(fruits);
+console.log(fruits.length);
