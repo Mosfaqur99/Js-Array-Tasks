1f413ad48decc05b6c4c1a7c64fb514d220a19bf
Mosfaqur Rahman <mosfaqur99@gmail.com>
2025-02-11 12:44:21 +0600

    Add or Remove

--- Add_or_remove_elements.js
+++ Add_or_remove_elements.js
@@ -3,5 +3,10 @@ var place = ["Cox's Bazar","Bali","Maldives"];
 place.push("Malaysia");
 console.log(place);
 
+// To add some data into your array.
 place.push("Rome","Tokyo")
 console.log(place);
+
+// To remove last data from your array.
+place.pop();
+console.log(place)
