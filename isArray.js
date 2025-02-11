953ff3286e9a79470418a3df4f7a23eac5cd0a33
Mosfaqur Rahman <mosfaqur99@gmail.com>
2025-02-11 14:43:12 +0600

    Is Array check

---
+++ isArray.js
@@ -0,0 +1,10 @@
+var cars = ["Marcedes Benz","Civic","Honda","Ford","Bugatti"];
+var Books = [];
+var Number = [10,25,32,35,45,74,82,79,89,94]
+var fruits = "mango"
+
+// to check is it an array or not
+console.log(Array.isArray(cars))
+console.log(Array.isArray(Books))
+console.log(Array.isArray(Number))
+console.log(Array.isArray(fruits))
