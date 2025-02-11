9de6a87e852dcdcfbf5bd193720f6fa0e08002a6
Mosfaqur Rahman <mosfaqur99@gmail.com>
2025-02-11 13:05:01 +0600

    Array Includes

---
+++ Includes.js
@@ -0,0 +1,11 @@
+var Books = ["Python","C","C++","Java","PHP"];
+
+Books.push("javascript")
+console.log(Books)
+
+// to check is this element available in the array
+console.log(Books.includes("javascript"));
+
+
+// to check what is the index number of the element
+console.log(Books.indexOf("javascript"))
